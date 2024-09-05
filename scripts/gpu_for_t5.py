986dd13cdf78097143a9b6f11a33ed83efe5981f
Juqowel <tropy100@gmail.com>
2024-09-05 09:50:01 +0400

    Small UI update

--- scripts/gpu_for_t5.py
+++ scripts/gpu_for_t5.py
@@ -1,6 +1,7 @@
 import torch
 import gradio as gr
 from modules import scripts
+from modules.ui_components import InputAccordion
 from backend import memory_management
 
 lowvram_available = memory_management.lowvram_available
@@ -13,14 +14,18 @@ class T5onOtherDevice(scripts.Script):
     def show(self, is_img2img):
         return scripts.AlwaysVisible
 
-    def ui(self, *args, **kwargs):
-        gpus = [f'cuda:{i}' for i in range(0, torch.cuda.device_count())]
+    def ui(self, is_img2img):
+        current_gpu = memory_management.get_torch_device()
+        gpus = [(torch.cuda.get_device_name(f"cuda:{i}"),f'cuda:{i}') for i in range(0, torch.cuda.device_count())]
+
         if len(gpus) < 2:
              return []
-        gpus.remove(f'{memory_management.get_torch_device()}')
-        with gr.Accordion(open=False, label=self.title()):
-            enabled = gr.Checkbox(label='Enabled', value=False)
-            choise = gr.Radio(label='Select GPU', choices=gpus, value=gpus[0])
+        
+        gpus.remove((torch.cuda.get_device_name(current_gpu),f'{current_gpu}'))
+        
+        with InputAccordion(False, label="T5 on Other Device", elem_id="T5onOtherDevice_enabled"+f"{is_img2img}") as enabled:
+            choise = gr.Radio(label='Select GPU', choices=gpus, value=gpus[0][1])
+            enabled.change(lambda i: print(f"{ f'T5 on Other Device is enabled for' if i else f'T5 on Other Device is disabled for'}{' img2img' if is_img2img else ' txt2img'} and {choise.value}"), inputs=[enabled], outputs=[])
         return [choise, enabled]
 
     def process(self, p, *script_args, **kwargs):
