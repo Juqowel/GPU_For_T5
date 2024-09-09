6ce9fdcf3c80b083ab401e88ccfaa7b35207d7f6
Juqowel <tropy100@gmail.com>
2024-09-09 08:23:34 +0400

    Add CPU option

--- scripts/gpu_for_t5.py
+++ scripts/gpu_for_t5.py
@@ -16,16 +16,18 @@ class T5onOtherDevice(scripts.Script):
 
     def ui(self, is_img2img):
         current_gpu = memory_management.get_torch_device()
-        gpus = [(torch.cuda.get_device_name(f"cuda:{i}"),f'cuda:{i}') for i in range(0, torch.cuda.device_count())]
+        device_list = [(torch.cuda.get_device_name(f"cuda:{i}"),f'cuda:{i}') for i in range(0, torch.cuda.device_count())]
+        device_list.append(('CPU', 'cpu'))
 
-        if len(gpus) < 2:
+        if len(device_list) < 2:
              return []
         
-        gpus.remove((torch.cuda.get_device_name(current_gpu),f'{current_gpu}'))
+        device_list.remove((torch.cuda.get_device_name(current_gpu),f'{current_gpu}'))
         
         with InputAccordion(False, label="T5 on Other Device", elem_id="T5onOtherDevice_enabled"+f"{is_img2img}") as enabled:
-            choise = gr.Radio(label='Select GPU', choices=gpus, value=gpus[0][1])
-            enabled.change(lambda i: print(f"{ f'T5 on Other Device is enabled for' if i else f'T5 on Other Device is disabled for'}{' img2img' if is_img2img else ' txt2img'} and {choise.value}"), inputs=[enabled], outputs=[])
+            choise = gr.Radio(label='Select device', choices=device_list, value=device_list[0][1])
+            enabled.change(lambda i, c: print(f"{ f'T5 on Other Device is enabled for' if i else f'T5 on Other Device is disabled for'}{' img2img' if is_img2img else ' txt2img'} and {c}"), inputs=[enabled, choise], outputs=[])
+            choise.change(lambda i, c: print(f"{f'T5 on Other Device is enabled for' if i else f'T5 on Other Device is disabled for'} {'img2img' if is_img2img else 'txt2img'} and {c}"), inputs=[enabled, choise], outputs=[])
         return [choise, enabled]
 
     def process(self, p, *script_args, **kwargs):
